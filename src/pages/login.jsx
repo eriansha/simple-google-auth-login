e439e2d9e5e06693bc0e03ddf18e02160640f6a7
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-22 17:22:04 +0700

    Modify login and signup page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Label, Input, Card, CardBody } from 'reactstrap';
 import GoogleLogin from 'react-google-login';
 
 class Login extends Component {
@@ -17,39 +18,45 @@ class Login extends Component {
 
   render() {
     return(
-      <Form>
-        <h3>Sign In</h3>
-        <FormGroup>
-          <Label for="email">Email address</Label>
-          <Input type="email" name="email" id="email" placeholder="Enter Email" autoFocus />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input type="password" name="password" id="password" placeholder="Enter Password" />
-        </FormGroup>
-        <FormGroup check>
-          <Label style={{ fontWeight: 'normal', marginBottom: '20px' }} check>
-            <Input type="checkbox" /> Remember me
-          </Label>
-        </FormGroup>
-        <Button color="primary" size="md" block>Submit</Button>
-        <FormGroup>
-          <div className="google-login">
-            <h6>Or</h6>
-            <GoogleLogin            
-              clientId={"658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"}
-              responseType={'id_token'}
-              buttonText="Login by Google"
-              onSuccess={this.handleSuccessGoogle}
-              onFailure={this.handleFailedGoogle}
-              cookiePolicy={'single_host_origin'}
-            />
-          </div>
-        </FormGroup>
-        <p className="forgot-password text-right">
-            Forgot <a href="/">password?</a>
-        </p>
-      </Form>
+      <Card>
+        <CardBody>
+          <Form>
+            <h3>Sign In</h3>
+            <FormGroup>
+              <Label for="email">Email address</Label>
+              <Input type="email" name="email" id="email" placeholder="Enter Email" autoFocus />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" id="password" placeholder="Enter Password" />
+            </FormGroup>
+            <FormGroup check>
+              <Label style={{ fontWeight: 'normal', marginBottom: '20px' }} check>
+                <Input type="checkbox" /> Remember me
+              </Label>
+            </FormGroup>
+            <Button color="primary" size="md" block>Submit</Button>
+            <FormGroup>
+              <div className="google-login">
+                <h6>Or</h6>
+                <GoogleLogin            
+                  clientId={"658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"}
+                  responseType={'id_token'}
+                  buttonText="Login by Google"
+                  onSuccess={this.handleSuccessGoogle}
+                  onFailure={this.handleFailedGoogle}
+                  cookiePolicy={'single_host_origin'}
+                />
+                <Link style={{ marginTop: '20px' }} to="/">Forget Password?</Link>
+              </div>
+            </FormGroup>
+            <p className="forgot-password text-right">
+                {/* Forgot <a href="/">password?</a> */}
+                Dont have account? <Link to="/sign-up">Sign Up</Link>
+            </p>
+          </Form>
+        </CardBody>
+      </Card>
     )
   }
 }
--- src/pages/sign-up.jsx
+++ src/pages/sign-up.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { Button, Form, FormGroup, Label, Input, Card, CardBody } from 'reactstrap';
 
 class SignUp extends Component {
   constructor(props) {
@@ -8,29 +8,33 @@ class SignUp extends Component {
 
   render() {
     return(
-      <Form>
-        <h3>Sign Up</h3>
-        <FormGroup>
-          <Label for="name">Name</Label>
-          <Input type="text" name="name" id="exampleEmail" placeholder="Enter name" autoFocus/>
-        </FormGroup>
-        <FormGroup>
-          <Label for="email">Email address</Label>
-          <Input type="email" name="email" id="email" placeholder="Enter Email" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="password">Password</Label>
-          <Input type="password" name="password" id="password" placeholder="Enter Password" />
-        </FormGroup>
-        <FormGroup>
-          <Label for="passwordConfirmation">Password Confirmation</Label>
-          <Input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter Password" />
-        </FormGroup>
-        <Button color="primary" size="md" block>Submit</Button>
-        <p className="forgot-password text-right">
-            Already registered <a href="/">sign in?</a>
-        </p>
-      </Form>
+      <Card>
+        <CardBody>
+          <Form>
+            <h3>Sign Up</h3>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input type="text" name="name" id="exampleEmail" placeholder="Enter name" autoFocus/>
+            </FormGroup>
+            <FormGroup>
+              <Label for="email">Email address</Label>
+              <Input type="email" name="email" id="email" placeholder="Enter Email" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Password</Label>
+              <Input type="password" name="password" id="password" placeholder="Enter Password" />
+            </FormGroup>
+            <FormGroup>
+              <Label for="passwordConfirmation">Password Confirmation</Label>
+              <Input type="password" name="passwordConfirmation" id="passwordConfirmation" placeholder="Enter Password" />
+            </FormGroup>
+            <Button color="primary" size="md" block>Submit</Button>
+            <p className="forgot-password text-right">
+                Already registered <a href="/sign-in">sign in?</a>
+            </p>
+          </Form>
+        </CardBody>
+      </Card>
     )
   }
 }
