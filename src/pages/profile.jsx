5e03b52198027e0b7ea75b4b66a166347633aa79
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-22 16:59:27 +0700

    Create profile page

---
+++ src/pages/profile.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    return(
+      <div>
+        Profile
+      </div>
+    )
+  }
+}
+
+export default Home;
