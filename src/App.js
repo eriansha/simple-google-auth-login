0f29f63a9963a3aaadb2138e9a62a16a495c9dd1
Ivan Putra Eriansya <eriansha.van@gmail.com>
2020-08-22 16:59:45 +0700

    Implement private route and auth provider in App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import './App.css';
+import PrivateRoute from './components/routes/PrivateRoute';
 
 import Login from './pages/login'
 import SignUp from './pages/sign-up'
 import Home from './pages/home'
+import Profile from './pages/profile'
 
-function App() {
+import { AuthContext } from './context/auth'
+
+function App(props) {
   return (
-    <Router>
-      <div className="App">
-        <Navbar color="faded" light>
-          <NavbarBrand href="/">React Login</NavbarBrand>
-          <Nav right>
-            <NavItem>
-              <NavLink href="/sign-in">Sign In</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="/sign-up">Sign Up</NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
+    <AuthContext.Provider value={false}>
+      <Router>
+        <div className="App">
+          <Navbar color="faded" light>
+            <NavbarBrand href="/">React Login</NavbarBrand>
+            <Nav right>
+              <NavItem>
+                <NavLink href="/sign-in">Sign In</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/sign-up">Sign Up</NavLink>
+              </NavItem>
+            </Nav>
+          </Navbar>
 
-        <div className="auth-wrapper">
-          <div className="auth-inner">
-            <Switch>
-              <Route exact path='/' component={Login} />
-              <Route path="/sign-in" component={Login} />
-              <Route path="/sign-up" component={SignUp} />
-              <Route path="/home" component={Home} />
-            </Switch>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/sign-in" component={Login} />
+                <Route path="/sign-up" component={SignUp} />
+                <PrivateRoute path="/profile" component={Profile} />
+              </Switch>
+            </div>
           </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </AuthContext.Provider>
   );
 }
 
